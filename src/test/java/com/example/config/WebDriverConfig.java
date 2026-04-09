6dcb38315412fecf68db4d7e03596ff4dbd9a1d0
Andrei.Grib_ext <Andrei.Grib_ext@external.mail.schwarz>
2026-04-09 11:48:11 +0300

    add Jenkins config

--- src/test/java/com/example/config/WebDriverConfig.java
+++ src/test/java/com/example/config/WebDriverConfig.java
@@ -107,7 +107,7 @@ public class WebDriverConfig {
                 driverSupplier = () -> {
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
-                    options.addArguments("--headless=new");
+                    options.addArguments("--headless=true");
                     options.addArguments("--start-maximized");
                     log.info("Launching Chrome with GUI");
                     return new ChromeDriver(options);
